cb3aa8bf1ffb1e281ae6d4f94131222d3702dbc4
Yilda <yildavglz@gmail.com>
2020-10-13 00:21:44 -0500

    learning react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+import CounterExample from './components/CounterExample'
+
+function App() {
+  return (
+   <div>
+     <HelloWorld name="John"/>
+
+   </div>
+  );
+}
+
+export default App;
